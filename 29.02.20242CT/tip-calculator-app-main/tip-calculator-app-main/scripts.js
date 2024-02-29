aa8e55968cd862447dddb96e309ef6449a49d571
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2024-02-29 10:33:21 +0100

    Add files via upload

---
+++ 29.02.20242CT/tip-calculator-app-main/tip-calculator-app-main/scripts.js
@@ -0,0 +1,9 @@
+function reset(){
+    document.getElementById("HRTDolar").innerText = "$0";
+    document.getElementById("bill").innerText = "0";
+    document.getElementById("custom").innerText = "custom";
+    document.getElementById("people").innerText = "0";
+}
+function proc5(){
+    
+}
